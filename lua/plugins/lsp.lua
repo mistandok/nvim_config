f00abbc0cc54536dca6d1deb3688c3de329af98b
akartikov <akartikov@avito.ru>
2024-11-09 10:47:36 +0300

    добавил LSP для брифа

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,55 @@
 return {
 	{
 		"neovim/nvim-lspconfig",
+		dependencies = {
+			"shrsh7th/cmp-nvim-lsp",
+		},
 		config = function()
 			local lspconfig = require("lspconfig")
+			local configs = require("lspconfig.configs")
+			local util = require("lspconfig.util")
+
+			vim.filetype.add({ extension = { brief = "brief" } })
+
+			-- Inject Brief LSP
+			if not configs.briefls then
+				configs.briefls = {
+					default_config = {
+						cmd = { "briefls" },
+						filetypes = { "brief" },
+						root_dir = function(fname)
+							return util.root_pattern(".git")(fname)
+						end,
+						single_file_support = true,
+						capabilities = {
+							workspace = {
+								didChangeWatchedFiles = {
+									dynamicRegistration = true,
+								},
+							},
+						},
+					},
+					settings = {},
+				}
+			end
+
+			-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+			--local capabilities = vim.lsp.protocol.make_client_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 			lspconfig.lua_ls.setup({})
 			lspconfig.gopls.setup({})
 			lspconfig.ts_ls.setup({})
 			lspconfig.pyright.setup({})
+
+			lspconfig.briefls.setup({
+				flags = {
+					debounce_text_changes = 150,
+				},
+				capabilities = capabilities,
+			})
+			--vim.lsp.start({ cmd = { "briefls" } })
+
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(ev)
@@ -21,7 +64,7 @@ return {
 					vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, opts)
 					vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename, { buffer = ev.buf, desc = "Rename Symbol" })
 					vim.keymap.set({ "n", "v" }, "<Leader>la", vim.lsp.buf.code_action, opts) -- различные действия с кодом, к примеру импорты и т.п.
-					vim.keymap.set("n", "<Leader>lf", function()               -- форматирование кода.
+					vim.keymap.set("n", "<Leader>lf", function() -- форматирование кода.
 						vim.lsp.buf.format({ async = true })
 					end, opts)
 				end,
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,13 +2,26 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		config = function()
+			vim.filetype.add({ extension = { brief = "brief" } })
+
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "go", "lua", "typescript", "javascript", "python", "json" },
+				ensure_installed = { "go", "lua", "typescript", "javascript", "python", "json", "brief" },
 				auto_install = true,
 				highlight = {
 					enable = true,
 				},
 			})
+
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			parser_config.brief = {
+				install_info = {
+					url = "/Users/akartikov/work/vim/tree-sitter-brief", -- local path or git repo
+					files = { "src/parser.c" },
+				},
+				filetype = "brief", -- if filetype does not match the parser name
+			}
+
+			vim.treesitter.language.register("brief", "brief")
 		end,
 	},
 }
