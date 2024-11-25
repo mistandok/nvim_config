9a2070394001253c0f7b504765ca9cea6660e173
akartikov <akartikov@avito.ru>
2024-11-25 13:41:59 +0300

    добавил цвета для indent

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,10 +3,27 @@
 return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
+		dependencies = "HiPhish/rainbow-delimiters.nvim",
 		config = function()
+			local highlight = {
+				"RainbowDelimiterRed",
+				"RainbowDelimiterYellow",
+				"RainbowDelimiterBlue",
+				"RainbowDelimiterOrange",
+				"RainbowDelimiterGreen",
+				"RainbowDelimiterViolet",
+				"RainbowDelimiterCyan",
+			}
+
+			vim.g.rainbow_delimeters = { highlight = highlight }
+
+			local hooks = require("ibl.hooks")
+			hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
 			require("ibl").setup({
+				scope = { highlight = highlight },
 				indent = {
-					char = "╏",
+					char = "┊",
 				},
 				exclude = {
 					filetypes = {
