f87c247d1c995e201e60dfa9a719b4fa5803336d
akartikov <akartikov@avito.ru>
2024-11-25 13:26:50 +0300

    добавил вывод пути, добавил цветные скобки, поправил, dashboard

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,8 +1,34 @@
+-- https://github.com/lukas-reineke/indent-blankline.nvim/blob/master/doc/indent_blankline.txt
+
 return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		config = function()
-			require("ibl").setup()
+			require("ibl").setup({
+				indent = {
+					char = "╏",
+				},
+				exclude = {
+					filetypes = {
+						"lspinfo",
+						"packer",
+						"checkhealth",
+						"help",
+						"man",
+						"gitcommit",
+						"TelescopePrompt",
+						"TelescopeResult",
+						"dashboard",
+					},
+					buftypes = {
+						"terminal",
+						"nofile",
+						"quickfix",
+						"prompt",
+						"dashboard",
+					},
+				},
+			})
 		end,
 	},
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -40,7 +40,7 @@ return {
 				},
 				sections = {
 					lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
-					lualine_b = { "filename", "branch" },
+					lualine_b = { { "filename", path = 1 }, "branch" },
 					lualine_c = {
 						"%=", --[[ add your center compoentnts here in place of this comment ]]
 					},
